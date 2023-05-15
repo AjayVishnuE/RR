d98f4b08a5a710de48888a0858fcba817c5c4964
AjayVishnuE <ajayvishnu121@gmail.com>
2023-05-15 23:11:04 +0530

    adding assets and navbar

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Navbar } from './navbar/Navbar';
+export { default as Scrollbar } from './scrollbar/Scrollbar';
+export { default as Sec1 } from './sec1/Sec1';
+export { default as Sec2 } from './sec2/Sec2';
+export { default as Sec3 } from './sec3/Sec3';
+export { default as Sec4 } from './sec4/Sec4';
+export { default as Sec5 } from './sec5/Sec5';
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,10 +3,29 @@ import './navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="Navbar">
-        <h2>Navbar</h2>
-      </div>
+    <div class="nav">
+        <input type="checkbox" id="nav-check"/>
+        <div class="nav-header">
+          <div class="nav-title">
+            <a href="./index.html">
+              <img class="nav-logo-desktop" src="./images/Renegades logo main.svg"/>
+              <img class="nav-logo-mobileview" src="./images/Renegades logo mobile.svg"/>
+            </a>
+          </div>
+        </div>
+        <div class="nav-btn">
+            <label for="nav-check">
+              <span></span>
+              <span></span>
+              <span></span>
+            </label>
+          </div>
+          <div class="nav-links">
+              <a href="">ABOUT GAME</a>
+              <a href="">WEB 3</a>
+              <a href="">GAME TESTING</a>
+              <a href="">CONTACT</a>
+          </div>
     </div>
   )
 }
