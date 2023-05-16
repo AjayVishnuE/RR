20e91359de36f523db7df197e5691d70603ab576
AjayVishnuE <ajayvishnu121@gmail.com>
2023-05-16 23:37:00 +0530

    section additions

--- src/components/sec4/Sec4.jsx
+++ src/components/sec4/Sec4.jsx
@@ -4,9 +4,21 @@ import './sec4.css';
 const Sec4 = () => {
   return (
     <div>
-      <div className="Sec4">
-        <h2>Sec4</h2>
-      </div>
+            <div class="section4">
+                <div class="section4-overlay">
+                </div>
+                <div class="section4-content">
+                    <h1 style={{color:"#FF723E", fontWeight: "600", fontSize:63, textAlign: "left", margin:"10px 0px", lineHeight: 1}}>The standard Lorem Ipsum<br/>passage from 1500</h1>
+                    <p style={{fontSize: "larger", textAlign: "left", color: "white", padding: "0 300px 0 0"}}>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words.</p>
+                    <div class="button-set">
+                        <a class="a-button" href="">
+                            <div class="special-button">
+                                <p style={{color:"black", fontWeight: "500"}}>VIEW PHOTO GALLERY</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
     </div>
   )
 }
--- src/components/sec5/Sec5.jsx
+++ src/components/sec5/Sec5.jsx
@@ -4,8 +4,20 @@ import './sec5.css';
 const Sec5 = () => {
   return (
     <div>
-      <div className="Sec5">
-        <h2>Sec5</h2>
+      <div class="section5">
+          <div class="section5-overlay">
+          </div>
+          <div class="section5-content">
+              <h1 style={{color:"#FF723E", fontWeight: "600", fontSize:63, textAlign: "left", margin:"10px 0px", lineHeight: 1}}>VIDEO GALLERY: <br/> GAMEPLAY + TRAILERS</h1>
+              <p style={{fontSize: "larger", textAlign: "left", color: "white", padding: "0 300px 0 0"}}>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate.</p>
+              <div class="button-set">
+                  <a class="a-button" href="">
+                      <div class="special-button">
+                          <p style={{color:"black", fontWeight: "500"}}>VIEW VIDEO GALLERY</p>
+                      </div>
+                  </a>
+              </div>
+          </div>
       </div>
     </div>
   )
