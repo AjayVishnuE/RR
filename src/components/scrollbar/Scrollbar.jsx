763a6dba9954532f53ad5dc4eb47bdf63a7304fc
AjayVishnuE <ajayvishnu121@gmail.com>
2023-05-16 23:59:38 +0530

    Scroll bar adding in jsx

--- src/components/scrollbar/Scrollbar.jsx
+++ src/components/scrollbar/Scrollbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './scrollbar.css';
 
+import progressBar from "./imports";
+
 const Scrollbar = () => {
   return (
     <div class="progress-container">
         <div id="progressbar" class="progress-bar"></div>
+        <progressBar/>
     </div>
   )
 }
